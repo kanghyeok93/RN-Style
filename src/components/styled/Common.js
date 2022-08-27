51185b5356360ccfcecb127bdc58690dd8486a24
smc p-n0055 <kanghyeok93@naver.com>
2022-08-27 15:59:45 +0900

    feat: Styling component structure #1

---
+++ src/components/styled/Common.js
@@ -0,0 +1,38 @@
+export const colors = {};
+
+// background color
+export const BACKGROUND_COLOR = props => {};
+
+// border color
+export const BORDER_COLOR = props => {};
+
+// font color
+export const FONT_COLOR = props => {};
+
+// margin
+export const MARGIN_TOP = props => {
+  return props.marginTop || '0';
+};
+export const MARGIN_LEFT = props => {
+  return props.marginLeft || '0';
+};
+export const MARGIN_RIGHT = props => {
+  return props.marginRight || '0';
+};
+export const MARGIN_BOTTOM = props => {
+  return props.marginBottom || '0';
+};
+
+// padding
+export const PADDING_TOP = props => {
+  return props.paddingTop || '0';
+};
+export const PADDING_LEFT = props => {
+  return props.paddingLeft || '0';
+};
+export const PADDING_RIGHT = props => {
+  return props.paddingRight || '0';
+};
+export const PADDING_BOTTOM = props => {
+  return props.paddingBottom || '0';
+};
---
+++ src/screen/SCScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const SCScreen = () => {
+  return (
+    <View>
+      <Text>SCScreen</Text>
+    </View>
+  );
+};
+
+export default SCScreen;
