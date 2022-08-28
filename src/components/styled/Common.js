687cc783b230bdad20c449f8a4334e095d1cd1d5
smc p-n0055 <kanghyeok93@naver.com>
2022-08-28 15:18:14 +0900

    feat: Styling component structure #2

--- src/components/styled/Common.js
+++ src/components/styled/Common.js
@@ -1,4 +1,13 @@
-export const colors = {};
+export const colors = {
+  Danger: '#ff4444',
+  Warning: '#ffbb33',
+  Success: '#00C851',
+  Info: '#33b5e5',
+  DangerDark: '#CC0000',
+  WarningDark: '#FF8800',
+  SuccessDark: '#007E33',
+  InfoDark: '#0099CC',
+};
 
 // background color
 export const BACKGROUND_COLOR = props => {};
@@ -35,4 +44,46 @@ export const PADDING_RIGHT = props => {
 };
 export const PADDING_BOTTOM = props => {
   return props.paddingBottom || '0';
+};
+
+// border width
+export const BORDER_LEFT_WIDTH = props => {
+  return props.borderLeftWidth || '1px';
+};
+export const BORDER_RIGHT_WIDTH = props => {
+  return props.borderRightWidth || '1px';
+};
+export const BORDER_BOTTOM_WIDTH = props => {
+  return props.borderBottomWidth || '1px';
+};
+export const BORDER_TOP_WIDTH = props => {
+  return props.borderTopWidth || '1px';
+};
+
+// font size
+export const FONT_SIZE = props => {
+  if (props.ftVerySmall) {
+    return '8px';
+  }
+  if (props.ftSmall) {
+    return '11px';
+  }
+  if (props.ftLarge) {
+    return '18px';
+  }
+  if (props.ftBigLarge) {
+    return '28px';
+  }
+  if (props.fontSize) {
+    return props.fontSize;
+  }
+  return '14px';
+};
+
+// font weight
+export const FONT_WEIGHT = props => {
+  if (props.bold) {
+    return 'Bold';
+  }
+  return 'Regular';
 };
