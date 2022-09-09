127e61460f734132fe70338f64c229082a85f4b6
smc p-n0055 <kanghyeok93@naver.com>
2022-09-09 21:35:49 +0900

    feat: Styling component structure #5

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View} from 'react-native';
 import BasicScreen from './src/screen/BasicScreen';
 import StyledBasicScreen from './src/screen/StyledBasicScreen';
+import SCScreen from './src/screen/SCScreen';
 
 const App = () => {
   return (
--- src/components/styled/View.js
+++ src/components/styled/View.js
@@ -1,1 +1,31 @@
 import styled from 'styled-components/native';
+import * as Common from './Common';
+
+export const View = styled.View(props => ({
+  // flex
+  flex: props.flex,
+  alignItems: props.alignItems || 'stretch',
+  justifyContent: props.justifyContent || 'flex-start',
+
+  // size
+  width: props.width || 'auto',
+  height: props.height || 'auto',
+
+  // color
+  backgroundColor: Common.BACKGROUND_COLOR(props),
+
+  // margin
+  marginLeft: Common.MARGIN_LEFT(props),
+  marginRight: Common.MARGIN_RIGHT(props),
+  marginTop: Common.MARGIN_TOP(props),
+  marginBottom: Common.MARGIN_BOTTOM(props),
+
+  // padding
+  paddingLeft: Common.PADDING_LEFT(props),
+  paddingRight: Common.PADDING_RIGHT(props),
+  paddingTop: Common.PADDING_TOP(props),
+  paddingBottom: Common.PADDING_BOTTOM(props),
+
+  // zIndex
+  zIndex: props.zIndex || 0,
+}));
